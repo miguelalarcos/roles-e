e842ae3807f3cd997ed70578c4ab71c227d715b3
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-02-22 19:49:17 +0100

    package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'miguelalarcos:roles-e',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'A simple role package for Meteor with multiple role inheritance',
   git: 'https://github.com/miguelalarcos/roles-e.git',
   documentation: 'README.md'
@@ -11,11 +11,9 @@ Package.onUse(function(api) {
   api.use('coffeescript', 'server');
   api.use('mongo', 'server');
   api.use('jquery', 'server');
-  api.use('ongoworks:security', 'server'); //1.0.1
-  api.use('accounts-base', ['client', 'server']);
-  //api.use('accounts-password', ['client', 'server']);
+  api.use('ongoworks:security@1.0.1', 'server');
+  api.use('accounts-base', 'server');
   api.addFiles('roles-e.coffee', 'server');
   api.export('roleE', 'server');
-
 });
 
